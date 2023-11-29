3273f63a9ab61be418a198392d04ba47d306bd47
Dev Mehta <143238208+devmehta1234@users.noreply.github.com>
2023-11-29 10:45:50 +0530

    Delete script13.php

--- script13.php
+++
@@ -1,6 +0,0 @@
-<?php
-        $linkText = "Click here";
-        $linkUrl = "https://www.example.com";
-        $anchorTag = '<a href="' . $linkUrl . '">' . $linkText . '</a>';
-        echo $anchorTag;
-?>
